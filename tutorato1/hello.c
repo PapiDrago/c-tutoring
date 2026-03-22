08eaff244ca8e0ea93876436de1ec7fbf827ab96
Claudio Guarrasi <claudio.guarrasi@gmail.com>
2026-03-22 17:49:20 +0100

    create hello file

---
+++ tutorato1/hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(void) {
+	puts("Hello world!!!"); // 'puts' automatically prints a trailing newline character "\n" (newline)
+	printf("Hello world!!!\n"); // I had to write \n explicitely
+	
+}

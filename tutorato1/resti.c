a3774fc3850de6c48b8b22da10a5910d2d0120f2
Claudio Guarrasi <claudio.guarrasi@gmail.com>
2026-03-23 14:28:23 +0100

    create 'resti.c' without using remainder operator

---
+++ tutorato1/resti.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void) {
+	int importo = 123;
+	printf("Importo totale: %d euro\n", importo);
+	int n_50 = importo / 50; 
+
+	importo = importo - n_50 * 50;
+
+	int n_20 = importo / 20;
+
+	importo = importo - n_20 * 20;
+
+	int n_10 = importo / 10;
+
+	importo = importo - n_10 * 10;
+
+	int n_5 = importo / 5;
+
+	importo = importo - n_5 * 5;
+
+	int n_2 = importo / 2;
+
+	importo = importo - n_2 * 2;
+
+	int n_1 = importo / 1;
+
+	importo = importo - n_1 * 1;
+
+	printf("%d banconote da 50 euro\n", n_50);
+	printf("%d banconote da 20 euro\n", n_20);
+	printf("%d banconote da 10 euro\n", n_10);
+	printf("%d banconote da 5 euro\n", n_5);
+	printf("%d monete da 2 euro\n", n_2);
+	printf("%d monete da 1 euro\n", n_1);
+
+	return 0;
+
+}
